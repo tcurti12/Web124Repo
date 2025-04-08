4083c224888acd0c3e9d2f1314df6154a705ab37
tcurti12 <tcurti12@stumail.jccc.edu>
2025-04-08 12:29:16 -0500

    Add files via upload

---
+++ Chapter3Project/chapter3project.js
@@ -0,0 +1,32 @@
+//Tanner Curtis, 4/8/2025, chapter3project.js
+
+const fullName = "Tanner Curtis";
+
+console.log(fullName);
+
+ const desiredSalary = "$1,000,000";
+
+ console.log(desiredSalary);
+
+ const vetStatus = "No";
+
+ console.log(vetStatus);
+
+ const friends = ["Jackson", "Alice", "Gabe"];
+
+ console.log(friends.join(", "));
+
+ const friendsSalary = ["$500,000", "$200,000", "$350,000"];
+
+ console.log(friendsSalary.join(", "));
+
+ const oneMoreFriend = {
+    firstName: "Jeremy",
+    lastName: "Albertson",
+    salary: "$10,000"
+ };
+
+ console.log(oneMoreFriend.firstName, oneMoreFriend.lastName);
+
+ console.log(oneMoreFriend.salary);
+
